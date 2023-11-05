bcd202647bd7a1d27e082f6ae18671b21b0e7ff3
KOH6 <minagk38+engineer@gmail.com>
2023-11-06 08:10:19 +0900

    registration時のパラメータ設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
-        include DeviseTokenAuth::Concerns::SetUserByToken
+  include DeviseTokenAuth::Concerns::SetUserByToken
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  # ユーザ登録時にデフォルトカラム以外を許可
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password, :password_confirmation, :phone, :birthdate])
+  end
 end
