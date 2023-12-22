869f509da8db990de42232cebf4a8b3fe5c2f286
KOH6 <minagk38+engineer@gmail.com>
2023-12-22 17:57:49 +0900

    プロフィールとヘッダーにdummy画像追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,35 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  before_save :attach_dummy_image
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
   include DeviseTokenAuth::Concerns::User
 
+  has_many :posts, dependent: :destroy
+  has_one_attached :profile_image
+  has_one_attached :header_image
+
   with_options presence: true do
     validates :phone
     validates :birthdate
     validates :name, uniqueness: true
+  end
+
+  private
+
+  def attach_dummy_image
+    unless profile_image.attached?
+      profile_image.attach(io: File.open(Rails.root.join('app/assets/images/dummy_image.jpg')),
+                   filename: 'dummy_image.jpg')
+    end
+
+    return if header_image.attached?
+
+    header_image.attach(io: File.open(Rails.root.join('app/assets/images/dummy_header_image.jpg')),
+                        filename: 'dummy_header_image.jpg')
   end
 end
