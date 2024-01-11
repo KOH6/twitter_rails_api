8575e666349fa9a622f16e0148c39bb83ef22e4f
KOH6 <minagk38+engineer@gmail.com>
2024-01-11 13:32:06 +0900

    ポストの本文に文字数制限を設定

--- db/migrate/20231111135027_create_posts.rb
+++ db/migrate/20231111135027_create_posts.rb
@@ -3,7 +3,7 @@
 class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
-      t.text :content, null: false
+      t.string :content, limit: 140, null: false
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
