584059e2f318ebfab01c1690a1b38719e90077c1
KOH6 <minagk38+engineer@gmail.com>
2024-01-13 10:28:05 +0900

    seedデータをよりランダムに修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,17 @@
 # frozen_string_literal: true
 
+# ランダム文字列を取得
+# 平仮名と常用漢字
+def self.rand_jp_str(length = 20)
+  char = "あいうえおかきくけこさしすせそたちつてとはひふへほはひふへほやゆよつわあいうえおかきくけこさしすせそたちつてとはひふへほはひふへほやゆよつわ亜哀挨愛曖悪握圧扱宛嵐安案暗以衣".split(//)
+  return Array.new(length){char[rand(1..char.size)]}.join
+end
+
+# アルファベット
+def self.rand_en_str(length = 5)
+  return ('a'..'z').to_a.sample(rand(1..length)).join
+end
+
 User.destroy_all
 
 USER_COUNT = 10
@@ -10,15 +22,15 @@ post_ids = []
 USER_COUNT.times do |n|
   count = n + 1
   user = User.new(
-    name: "ユーザ名#{count}#{('a'..'z').to_a.sample(8).join}",
-    user_name: "#{count}#{('a'..'z').to_a.sample(8).join}",
+    name: "ユーザ#{count}#{rand_en_str(8)}",
+    user_name: "#{count}#{rand_en_str(8)}",
     email: "example#{count}@example.com",
     password: "#{'a' * count}111111",
-    birthdate: '2000-01-01',
+    birthdate: "#{rand(1900..2010)}-#{rand(1..12)}-#{rand(1..20)}",
     phone: '111122223333',
-    introduction: '自己紹介文です' * 20,
-    place: '東京都',
-    website: 'google.com'
+    introduction: rand(2).zero? ? "自己紹介文です#{rand_jp_str(100)}" : '',
+    place: rand(2).zero? ? "東京都#{rand_jp_str(15)}" : '',
+    website: rand(2).zero? ? "google.com/#{rand_en_str(8)}" : ''
   )
   user.skip_confirmation!
   user.save!
@@ -53,3 +65,4 @@ user_ids.each do |user_id|
   followee_ids = user_ids.reject { |id| id == user_id }.sample(rand(USER_COUNT - 1))
   followee_ids.each { |followee_id| Follow.create!(follower_id: user_id, followee_id:) }
 end
+
