a88df3d5324c41ea3c439a02e9478509d49d419a
KOH6 <minagk38+engineer@gmail.com>
2024-01-16 00:25:50 +0900

    bookmarkAPIで返すjsonの構造を修正

--- app/controllers/api/v1/bookmarks_controller.rb
+++ app/controllers/api/v1/bookmarks_controller.rb
@@ -1,17 +1,17 @@
 class Api::V1::BookmarksController < ApplicationController
   def index
-    bookmarks = User.first.bookmarking_posts
-    render json: { data: bookmarks }
+    bookmarks = User.first.bookmarking_posts.map(&:merge_user_and_image_as_json)
+    render json: bookmarks
   end
 
   def create
     bookmark = User.first.bookmarks.create(post_id: params[:tweet_id])
-    render json: { data: bookmark }
+    render json: bookmark
   end
 
   def destroy
     bookmark = User.first.bookmarks.find_by(post_id: params[:tweet_id])
     bookmark.destroy
-    render json: { data: bookmark }
+    render json: bookmark
   end
 end
