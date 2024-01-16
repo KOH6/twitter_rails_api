92f1fc74e80c2ef99bc138df8faf96591fdf0ffe
KOH6 <minagk38+engineer@gmail.com>
2024-01-16 11:05:02 +0900

    bookmarkのユーザを認証情報から取得

--- app/controllers/api/v1/bookmarks_controller.rb
+++ app/controllers/api/v1/bookmarks_controller.rb
@@ -4,17 +4,17 @@ module Api
   module V1
     class BookmarksController < ApplicationController
       def index
-        bookmarks = User.first.bookmarking_posts.map(&:merge_user_and_image_as_json)
+        bookmarks = current_api_v1_user.bookmarking_posts.map(&:merge_user_and_image_as_json)
         render json: bookmarks
       end
 
       def create
-        bookmark = User.first.bookmarks.create(post_id: params[:tweet_id])
+        bookmark = current_api_v1_user.bookmarks.create(post_id: params[:tweet_id])
         render json: bookmark
       end
 
       def destroy
-        bookmark = User.first.bookmarks.find_by(post_id: params[:tweet_id])
+        bookmark = current_api_v1_user.bookmarks.find_by(post_id: params[:tweet_id])
         bookmark.destroy
         render json: bookmark
       end
